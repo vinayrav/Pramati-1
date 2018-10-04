0594b438eb5c26a3c0c8995dacc328060556cb28
Keerthana Manoharan <43063410+Keerthana786@users.noreply.github.com>
2018-10-04 18:28:53 +0530

    Update assignment2.js

--- Training/HTML_CSS_JS_JQ/assignment2/assignment2.js
+++ Training/HTML_CSS_JS_JQ/assignment2/assignment2.js
@@ -1,136 +1,116 @@
- $(document).ready(function() {
-            $("#datepicker").datepicker();
-            
-            
-             $("#fname").keyup(function () {  
-                $('#fname').css('textTransform', 'capitalize');  
-            });
-            $("#lname").keyup(function () {  
-                $('#lname').css('textTransform', 'capitalize');  
-            });
+$(document).ready(function() {
+  $("#datepicker").datepicker({
+    format: 'mm-dd-yyyy',
+    endDate: '+0d',
+    autoclose: true
+  });
+  $('#first_name').css('textTransform', 'capitalize');
+  $('#last_name').css('textTransform', 'capitalize');
+  var tale_row = 0;
+  var error_flag = 0;
+  var focus = 0;
+ 
+  
+  
+  $('#first_name').focusout(function() {
+    focus = 1;
+    $('#first_name_error').text("Invalid first name");
+    if ($('#first_name').val().length < 1 || !/^[a-zA-Z\' ']+$/.test($('#first_name').val())) {
+      $('#first_name_error').show();
+    } else {
+      $('#first_name_error').hide();
+    }
+  });
+  
+  $('#last_name').focusout(function() {
+    focus = 1;
+    $('#last_name_error').text("Invalid last name");
+    if ($('#last_name').val().length < 1 || !/^[a-zA-Z\' ']+$/.test($('#last_name').val())) {     
+      $('#last_name_error').show();
+    } else {    
+      $('#last_name_error').hide();
+    }
+  });
+  
+  $('#email').focusout(function() {
+    focus = 1;
+    $("#email_error").text("Invalid Email");
+    if ($('#email').val().length < 1 || !/^([a-zA-Z]+[\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($('#email').val())) {      
+      $("#email_error").show();
+    } else {     
+      $("#email_error").hide();
+    }
+  });
 
-        });
+  $('#phone_number').focusout(function() {
+    focus = 1; 
+    $("#phone_number_error").text("Invalid Mobile Number");  
+    if ($('#phone_number').val().length < 1 || !/^([6-9]+[\d]{9})?$/.test($('#phone_number').val())) {      
+      $("#phone_number_error").show();
+    } else {      
+      $("#phone_number_error").hide();
+    }
+  });
 
-        function val() {
-            var i = 0;
 
-            var flag = 0;
-            var bf = 0;
-            var fname = document.getElementById("fname").value;
-            var lname = document.getElementById("lname").value;
-            var email = document.getElementById("email").value;
-            var number = document.getElementById("number").value;
-            var dob = document.getElementById("datepicker").value;
+  $('#datepicker').focusout(function() {
+    focus = 1;
+    $('#dob_error').text("Invalid date of birth");   
+    if ($('#datepicker').val().length < 1) {      
+      $('#dob_error').show();
+    } else {      
+      $('#dob_error').hide();
+    }
+  });
 
-            if (fname.length < 1) {
-                document.getElementById("fs").innerHTML = "First Name cannot be blank";
-                $("#fs").show();
-                bf = 1;
-                flag = 1;
-            }
-            if (lname.length < 1) {
-                document.getElementById("ls").innerHTML = "Last Name cannot be blank";
-                 $("#ls").show();
-                bf = 1;
-                flag = 1;
-            }
+  function capitalize(name) {
+    var words = name.split(" ");
+    final_name = "";
+    var i = 0;
+    for (i = 0; i < words.length; i++) {
+      final_name += words[i].charAt(0).toUpperCase() + words[i].substring(1, words[i].length) + " ";
+    }
+    return final_name;
+  }
 
-            if (dob.length < 1) {
-                document.getElementById("ds").innerHTML = "DOB cannot be blank";
-                $("#ds").show();
-                bf = 1;
-                flag = 1;
-            }
-            if (email.length < 1) {
-
-                document.getElementById("es").innerHTML = "Email cannot be blank";
-                $("#es").show();
-                bf = 1;
-                flag = 1;
-            }
-            if (number.length < 1) {
-
-                document.getElementById("ns").innerHTML = "Phone Number cannot be blank";
-                $("#ns").show();
-                bf = 1;
-                flag = 1;
-            }
-
-            if (bf == 0) {
-                
-                $("#fs").hide();
-                $("#ls").hide();
-                $("#es").hide();
-                $("#ns").hide();
-                $("#ds").hide();
-                var today = new Date();
-                var given = new Date(dob);
-                if (given > today) {
-                    
-
-                    document.getElementById("ds").innerHTML = "DOB cannot be future date";
-                    $("#ds").show();
-                    flag = 1;
-                }
-                if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
-                    flag = 1;
-                    document.getElementById("es").innerHTML = "Enter a valid email address";
-                    $("#es").show();
-
-                }
-                if (!(/^[a-zA-Z]/.test(email))) {
-                    flag = 1;
-                    document.getElementById("es").innerHTML = "Email address cannot start with numbers";
-                    $("#es").show();
-
-                }
-
-                if (!(number.length == 10)) {
-                    flag = 1;
-                    document.getElementById("ns").innerHTML = "Invalid Phone Number";
-                    $("#ns").show();
-
-                }
-                if (number.length == 10 && !(number.charAt(0) == '6' || number.charAt(0) == '7' || number.charAt(0) == '8' || number.charAt(0) == '9')) {
-                    flag = 1;
-                    document.getElementById("ns").innerHTML = "Phone Number should start with 6,7,8,9 only";
-                    $("#ns").show();
-
-                }
-
-            }
-
-            if (flag == 0) {
-
-                var tab = document.getElementById("form_details");
-
-                var row = tab.insertRow(++i);
-                var c1 = row.insertCell(0);
-
-                var c2 = row.insertCell(1);
-                var c3 = row.insertCell(2);
-                var c4 = row.insertCell(3);
-                var c5 = row.insertCell(4);
-                fname = fname.charAt(0).toUpperCase() + fname.substring(1, fname.length);
-                lname = lname.charAt(0).toUpperCase() + lname.substring(1, lname.length);
-                c1.innerHTML = fname;
-                c2.innerHTML = lname;
-                c3.innerHTML = email;
-                c4.innerHTML = number;
-                c5.innerHTML = dob;
-                document.getElementById("sample").reset();
-                document.getElementById("fs").innerHTML = " ";
-                document.getElementById("ls").innerHTML = " ";
-                document.getElementById("es").innerHTML = " ";
-                document.getElementById("ns").innerHTML = " ";
-                document.getElementById("ds").innerHTML = " ";
-                $("#fs").hide();
-                $("#ls").hide();
-                $("#es").hide();
-                $("#ns").hide();
-                $("#ds").hide();
-
-            }
-
-        }
-    
+  $('#submit_button').click(function() {
+	if( $("#first_name_error").is(":visible") || $("#last_name_error").is(":visible") || $("#email_error").is(":visible") || $("#phone_number_error").is(":visible") || $("#dob_error").is(":visible") ){
+		error_flag=1;
+	}
+	else{
+		error_flag=0;
+	}	    
+    //alert(error_flag);
+    if (error_flag == 0 && focus == 1) {
+	  $("#first_name_error").hide();
+      $("#last_name_error").hide();
+      $("#email_error").hide();
+      $("#phone_number_error").hide();
+      $("#dob_error").hide();
+      var tab = $('#form_details_table');
+      var first_name = capitalize($('#first_name').val());
+      var last_name = capitalize($('#last_name').val());
+      var email = $('#email').val();
+      var phone_number = $('#phone_number').val();
+      var dob = $('#datepicker').val();
+      tab.append("<tr ><td class='text-center' >" + first_name + "</td><td class='text-center'>" + last_name + "</td><td class='text-center'>" + email + "</td><td class='text-center' >" + phone_number + "</td><td class='text-center'>" + dob + "</td></tr>");
+      $('#sample_form').trigger("reset");
+      focus=0
+    } else if(focus==0) {
+	  $('#first_name_error').text("Invalid first name");
+      $('#last_name_error').text("Invalid last name");
+      $("#email_error").text("Invalid Email");
+      $("#phone_number_error").text("Invalid Mobile Number");
+      $('#dob_error').text("Invalid date of birth");
+      $("#first_name_error").show();
+      $("#last_name_error").show();
+      $("#email_error").show();
+      $("#phone_number_error").show();
+      $("#dob_error").show();		
+      //alert("No Black fileds are allowed!");
+    }
+    else if(error_flag==1){
+		alert("Please correct the errors !");
+	}
+  });
+});
