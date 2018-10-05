0fcb304fc4731280b4c027e454b6f1717dd15369
Keerthana Manoharan <43063410+Keerthana786@users.noreply.github.com>
2018-10-05 14:44:43 +0530

    Update assignment2.js

--- Training/HTML_CSS_JS_JQ/assignment2/assignment2.js
+++ Training/HTML_CSS_JS_JQ/assignment2/assignment2.js
@@ -1,3 +1,36 @@
+function name_validation(name){
+	 if (name.length < 1 || !/^[a-zA-Z\' ']+$/.test(name))
+	 {
+		 return false;
+	 }else{
+		 return true;
+	 }
+ }
+function email_validation(email){
+	if (email.length < 1 || !/^([a-zA-Z]+[\d\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email))
+	{
+		return false;
+	}else{
+		return true;
+	}
+}
+function phone_number_validation(phone_number){
+	 if (phone_number.length < 1 || !/^([6-9]+[\d]{9})?$/.test(phone_number))
+	 {
+		 return false;
+	 }else{
+		 return true;
+	 }
+ }
+ 
+function date_of_birth_validation(dob){
+	if(dob.length < 1){
+		return true;
+	}else{
+		return false;
+	}	
+}	
+	
 $(document).ready(function() {
   $("#datepicker").datepicker({
     format: 'mm-dd-yyyy',
@@ -8,14 +41,15 @@ $(document).ready(function() {
   $('#last_name').css('textTransform', 'capitalize');
   var tale_row = 0;
   var error_flag = 0;
-  var focus = 0;
- 
-  
-  
+  var focus_firstname = 0;
+  var focus_lastname = 0; 
+  var focus_email = 0; 
+  var focus_phonenumber = 0;
+  var focus_dob = 0;   
   $('#first_name').focusout(function() {
-    focus = 1;
+    focus_firstname = 1;
     $('#first_name_error').text("Invalid first name");
-    if ($('#first_name').val().length < 1 || !/^[a-zA-Z\' ']+$/.test($('#first_name').val())) {
+    if (!name_validation($('#first_name').val())) {
       $('#first_name_error').show();
     } else {
       $('#first_name_error').hide();
@@ -23,9 +57,9 @@ $(document).ready(function() {
   });
   
   $('#last_name').focusout(function() {
-    focus = 1;
+    focus_lastname = 1;
     $('#last_name_error').text("Invalid last name");
-    if ($('#last_name').val().length < 1 || !/^[a-zA-Z\' ']+$/.test($('#last_name').val())) {     
+    if (!name_validation($('#last_name').val())) {     
       $('#last_name_error').show();
     } else {    
       $('#last_name_error').hide();
@@ -33,9 +67,9 @@ $(document).ready(function() {
   });
   
   $('#email').focusout(function() {
-    focus = 1;
+    focus_email = 1;
     $("#email_error").text("Invalid Email");
-    if ($('#email').val().length < 1 || !/^([a-zA-Z]+[\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($('#email').val())) {      
+    if (!email_validation($('#email').val())) {      
       $("#email_error").show();
     } else {     
       $("#email_error").hide();
@@ -43,9 +77,9 @@ $(document).ready(function() {
   });
 
   $('#phone_number').focusout(function() {
-    focus = 1; 
+    focus_phonenumber = 1; 
     $("#phone_number_error").text("Invalid Mobile Number");  
-    if ($('#phone_number').val().length < 1 || !/^([6-9]+[\d]{9})?$/.test($('#phone_number').val())) {      
+    if (!phone_number_validation($('#phone_number').val())) {      
       $("#phone_number_error").show();
     } else {      
       $("#phone_number_error").hide();
@@ -53,8 +87,8 @@ $(document).ready(function() {
   });
 
 
-  $('#datepicker').focusout(function() {
-    focus = 1;
+  $('#datepicker').focus(function() {
+    focus_dob = 1;
     $('#dob_error').text("Invalid date of birth");   
     if ($('#datepicker').val().length < 1) {      
       $('#dob_error').show();
@@ -81,7 +115,29 @@ $(document).ready(function() {
 		error_flag=0;
 	}	    
     //alert(error_flag);
-    if (error_flag == 0 && focus == 1) {
+    if(focus_firstname == 0 )
+     { 
+	    $('#first_name_error').text("Invalid first name");
+	    $("#first_name_error").show();
+     } if (focus_lastname==0 ){
+	    $('#last_name_error').text("Invalid last name");
+	    $("#last_name_error").show();
+	 } if (focus_phonenumber==0){
+		$("#phone_number_error").text("Invalid Mobile Number");
+		$("#phone_number_error").show();
+	 } if (focus_email==0){
+	    $("#email_error").text("Invalid Email"); 
+	    $("#email_error").show();
+	 } if (focus_dob==0){
+		$('#dob_error').text("Invalid date of birth"); 
+		$("#dob_error").show();
+	 }     		
+      //alert("No Black fileds are allowed!");
+    
+     if(error_flag==1){
+		alert("Please correct the errors !");
+	}
+	if (error_flag == 0 && focus_firstname == 1 && focus_lastname==1 && focus_phonenumber==1 && focus_email==1 && focus_dob==1) {
 	  $("#first_name_error").hide();
       $("#last_name_error").hide();
       $("#email_error").hide();
@@ -95,22 +151,12 @@ $(document).ready(function() {
       var dob = $('#datepicker').val();
       tab.append("<tr ><td class='text-center' >" + first_name + "</td><td class='text-center'>" + last_name + "</td><td class='text-center'>" + email + "</td><td class='text-center' >" + phone_number + "</td><td class='text-center'>" + dob + "</td></tr>");
       $('#sample_form').trigger("reset");
+      focus_firstname=0;
+      focus_lastname=0;
+      focus_email=0;
+      focus_phonenumber=0;
+      focus_dob=0;
       focus=0
-    } else if(focus==0) {
-	  $('#first_name_error').text("Invalid first name");
-      $('#last_name_error').text("Invalid last name");
-      $("#email_error").text("Invalid Email");
-      $("#phone_number_error").text("Invalid Mobile Number");
-      $('#dob_error').text("Invalid date of birth");
-      $("#first_name_error").show();
-      $("#last_name_error").show();
-      $("#email_error").show();
-      $("#phone_number_error").show();
-      $("#dob_error").show();		
-      //alert("No Black fileds are allowed!");
-    }
-    else if(error_flag==1){
-		alert("Please correct the errors !");
-	}
+    } 
   });
 });
